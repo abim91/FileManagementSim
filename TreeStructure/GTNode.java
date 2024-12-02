d1f47100ff50dbb1b83acd8448335a59dab21b9c
Ryder614 <161537181+Ryder614@users.noreply.github.com>
2024-12-02 05:38:35 -0500

    Update GTNode.java

--- TreeStructure/GTNode.java
+++ TreeStructure/GTNode.java
@@ -1,107 +1,84 @@
 package TreeStructure;
+
 import java.time.LocalDateTime;
 import java.util.*;
-public class GTNode{
+
+public class GTNode {
     private String name;
-    private String info;
-    private String fileExtension;
-    private List<GTNode> children; //directories aka the folders
     private boolean isAFile;
+    private List<GTNode> children; // directories aka the folders
     private LocalDateTime timestamp; // Timestamp when the node is created
     private LocalDateTime lastModifiedTimestamp;
-   // private GTNode parent;
+    private GTNode parent; // Parent directory reference
 
-    public GTNode(String name, boolean isAFile){
-      //this.info = info;
-      this.name = name;
-      this.isAFile = true;
-      this.timestamp = LocalDateTime.now(); // Set the timestamp when the node is created
-      this.lastModifiedTimestamp = this.timestamp;
-      
-      /*if(!isAFile){
+    // Constructor for file nodes
+    public GTNode(String name, boolean isAFile) {
+        this.name = name;
+        this.isAFile = isAFile;
+        this.timestamp = LocalDateTime.now();
+        this.lastModifiedTimestamp = this.timestamp;
         this.children = new ArrayList<>();
-      }*/
-     
+        this.parent = null;  // Initially, parent is null
     }
 
-    public GTNode getParent(){
-      return this;
+    // Constructor for directory nodes
+    public GTNode(String name) {
+        this.name = name;
+        this.isAFile = false;
+        this.timestamp = LocalDateTime.now();
+        this.lastModifiedTimestamp = this.timestamp;
+        this.children = new ArrayList<>();
+        this.parent = null;  // Initially, parent is null
     }
+
+    // Getter and setter for parent
+    public GTNode getParent() {
+        return parent;
+    }
+
+    public void setParent(GTNode parent) {
+        this.parent = parent;
+    }
+
+    public boolean isAFile() {
+        return isAFile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public List<GTNode> getChildren() {
-      return children; 
-  }
-  
-  
-
-    public GTNode(String name){
-    
-      this.name = name;
-      this.children = new ArrayList<>();
-      this.isAFile = false;
-      this.timestamp = LocalDateTime.now(); // Set the timestamp when the node is created
-      this.lastModifiedTimestamp = this.timestamp;
-     // this.parent = parent;
+        return children;
     }
 
-    
+    // Method to add children (sets parent automatically)
+    public void addChildren(GTNode child) {
+        children.add(child);
+        child.setParent(this); // Set this node as the parent for the child
+        setLastModifiedTimestamp();
+    }
+
+    public void removeChildren(String name) {
+        children.removeIf(child -> child.getName().equals(name));
+        setLastModifiedTimestamp();
+    }
+
     public String formattedTimestamp() {
-      return timestamp.toString(); // You can customize this format if needed
-  }
-  public String formattedLastModifiedTimestamp() {
-    return lastModifiedTimestamp.toString(); // You can customize the format
+        return timestamp.toString();
+    }
+
+    public String formattedLastModifiedTimestamp() {
+        return lastModifiedTimestamp.toString();
+    }
+
+    public void setLastModifiedTimestamp() {
+        this.lastModifiedTimestamp = LocalDateTime.now(); // Update last modified time whenever changes happen
+    }
+
+    // Override toString for easy printing of node details
+    @Override
+    public String toString() {
+        return "Node name: " + name + ", Created at: " + formattedTimestamp();
+    }
 }
-public void setLastModifiedTimestamp() {
-  this.lastModifiedTimestamp = LocalDateTime.now(); // Update last modified time whenever changes happen
-}
-
-    public void addChildren(GTNode c){
-      children.add(c);
-      setLastModifiedTimestamp();
-      System.out.println("Add child operation went successful");
-    }
-
-    public void removeChildren(String c){
-      if(children.size() == 0){
-        System.out.println("There is nothing to delete");
-      }
-      GTNode todelete = new GTNode(null,true);
-      for(GTNode i: children){
-        if(i.name.equals(c)){
-          todelete = i;;
-        }
-      }
-      children.remove(todelete);
-      setLastModifiedTimestamp();
-    }
-
-    public void listChildren(){
-        for(GTNode i : children){
-        System.out.println(i.name);
-        //System.out.println("List child operation went successful");
-        System.out.println(i.name + " (Created at: " + i.formattedTimestamp() + ")");
-      }
-    }
-
-    public GTNode validateExistance(String node){
-      for(GTNode i: children){
-        if( i.getName().equals(node)){
-          return i;
-        }
-      }
-      return null;
-    }
-    public String getName(){
-      return this.name;
-    }
-
-    public void addFile(String name){
-
-    }
-    public String toString(){
-      return "Node name: " + this.name + ", Created at: " + formattedTimestamp();
-    }
-    
-    
-    
-
-  }

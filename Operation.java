ba1efbb1a30965d898b817b27f718c981c2ec465
Ryder614 <161537181+Ryder614@users.noreply.github.com>
2024-12-02 05:37:31 -0500

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -1,36 +1,24 @@
-import java.util.Arrays;
-
 import TreeStructure.GTNode;
 
 public class Operation {
     private String command;
     private String[] parameters;
     private GTNode currentObject;
-    public Operation(String command){
-        this.command = command;
-    }
 
-
-    public Operation(String[] parameter){
+    public Operation(String[] parameter) {
         this.command = parameter[0];
         this.parameters = parameter;
-        
     }
 
-    public GTNode getCurrentDNode(){
-        return currentObject;
-    }
-
-     // Getter for command
-     public String getCommand() {
+    public String getCommand() {
         return command;
     }
 
-
-    // Getter for parameters
     public String[] getParameters() {
         return parameters;
     }
 
-
+    public GTNode getCurrentObject() {
+        return currentObject;
+    }
 }
